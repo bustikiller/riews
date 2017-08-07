5b69d08d09897e4f5ea3197d6ee4a4f73b8c92bc
Manuel Bustillo <mayn13@gmail.com>
2017-08-07 20:44:36 +0200

    Create basic view of raffle view

--- app/helpers/riews/views_helper.rb
+++ app/helpers/riews/views_helper.rb
@@ -1,4 +1,30 @@
 module Riews
   module ViewsHelper
+
+    def generate_view_content_for(view)
+      content_tag :table do
+        generate_header_for(view) + generate_body_for(view)
+      end
+    end
+
+    def generate_header_for(view)
+      content_tag :thead do
+        content_tag :tr do
+          view.columns.map {|column| content_tag :td, column}.inject(:+)
+        end
+      end
+    end
+
+    def generate_body_for(view)
+      content_tag :tbody do
+        view.results.map do |object|
+          content_tag :tr do
+            view.columns.map do |column_name|
+              content_tag :td, object[column_name]
+            end.inject(:+)
+          end
+        end.inject(:+)
+      end
+    end
   end
 end
--- app/models/riews/view.rb
+++ app/models/riews/view.rb
@@ -1,4 +1,11 @@
 module Riews
   class View < ApplicationRecord
+    def columns
+      model.constantize.attribute_names
+    end
+
+    def results
+      model.constantize.all
+    end
   end
 end
