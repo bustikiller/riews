c7f10918d999d91c1940daef344fd1a10b272df6
Manuel Bustillo <mayn13@gmail.com>
2017-08-20 17:02:00 +0200

    Remove test folder used with minitest

--- test/dummy/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount Riews::Engine => "/riews"
-end
--- test/models/riews/view_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module Riews
-  class ViewTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
