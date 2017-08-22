fecb732842ba692920aafe76ccda9505a677c6f9
Manuel Bustillo <mayn13@gmail.com>
2017-08-22 16:35:01 +0200

    Format replacement tokens as list

--- app/helpers/riews/columns_helper.rb
+++ app/helpers/riews/columns_helper.rb
@@ -3,5 +3,13 @@ module Riews
     def aggregation_functions_for_select
       Riews::Column.aggregation_functions.to_a
     end
+
+    def format_replacement_tokens(tokens)
+      content_tag :ul do
+        tokens.map do |k, v|
+          content_tag :li, "#{k} -> #{v[:description]}"
+        end.inject(:+)
+      end
+    end
   end
 end
