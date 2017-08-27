22703634209da7265c19d3c7cf57164d51b65b5e
Manuel Bustillo <mayn13@gmail.com>
2017-08-27 22:52:42 +0200

    Test #klass and #group_query methods in view model

--- spec/models/riews/view_spec.rb
+++ spec/models/riews/view_spec.rb
@@ -16,4 +16,40 @@ describe Riews::View, type: :model do
       it { should accept_nested_attributes_for(:relationships).allow_destroy(true) }
     end
   end
+
+  describe '#klass' do
+    it 'returns the class of the model the view is associated to' do
+      view = build :view, model: 'Object'
+      expect(view.klass).to eq Object
+    end
+  end
+
+  describe '#group_query' do
+    let(:view) do
+      view = build :view
+      allow(view).to receive(:available_columns){%w(lorem ipsum)}
+      view.save validate: false
+      view
+    end
+    let(:query) do
+      query = Object.new
+      allow(query).to receive(:group)
+      query
+    end
+    it 'calls the ActiveRecord::Relation method #group with the columns with GROUP aggregation' do
+      create :column, view: view, method: 'lorem', aggregate: Riews::Column.functions[:group]
+      create :column, view: view, method: 'ipsum', aggregate: Riews::Column.functions[:group]
+
+      expect(query).to receive(:group).with(%w(lorem ipsum))
+      view.send :group_query, query
+    end
+
+    it 'excludes the columns that do not have GROUP aggregation' do
+      create :column, view: view, method: 'lorem', aggregate: Riews::Column.functions[:group]
+      create :column, view: view, method: 'ipsum', aggregate: Riews::Column.functions[:max]
+
+      expect(query).to receive(:group).with(['lorem'])
+      view.send :group_query, query
+    end
+  end
 end
