8b0f31b90bdf885de32c73967da40008ab8f63b0
Manuel Bustillo <mayn13@gmail.com>
2017-09-01 18:55:08 +0200

    Enable UI sorting in columns form

--- app/assets/javascripts/riews/views.js
+++ app/assets/javascripts/riews/views.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function(){
+    $( ".sortable" ).sortable({
+        axis: 'y',
+        items: '.sortable-item',
+        handle: '.sort-handler'
+    });
+});
